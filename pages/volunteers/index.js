814e5a6c26b0ca41c420989418cd8b3389b8b06f
profnike <profnike@gmail.com>
2022-06-04 10:15:07 +0100

    initial-commit for volunteers page

--- pages/volunteers/index.js
+++ pages/volunteers/index.js
@@ -1,86 +1,111 @@
 import React from "react";
-import PageTitle from "../../components/shared/PageTitle";
+import InnerSection from "../../components/volunteer/InnerSection";
+
+import InnerSectionList from "../../components/volunteer/InnerSectionList";
+import SideBar from "../../components/shared/SideBar";
+import FixturesBrief from "../../components/shared/FixturesBrief";
+import LatestNews from "../../components/volunteer/LatestNews";
 import Image from "next/dist/client/image";
 
+const volunteer = () => {
+  return (
+    <div className=" ">
+      <div className="max-w-[94%] md:max-w-[90%] mx-auto  py-8 lg:pb-3 h-auto   ">
+        <div className="flex gap-5 xl:gap-8 justify-between ">
+          <div className="w-full lg:w-8/12 xl:w-9/12 ">
+            <div className=" pb-[10vh] md:h-[30%] ">
+              <div className="">
+                <div>
+                  <Image
+                    src="/Lets-start.png"
+                    alt=""
+                    width="100%"
+                    height="40vh"
+                    layout="responsive"
+                    objectFit="cover"
+                  />
+                </div>
+                <p className="text-black mt-8 leading-8">
+                  {" "}
+                  Lörem ipsum jen öling tede den geosorade lalig så sagen om terralog burkini om plagen, ifall vaktig
+                  inte leda krosäbelt. Myjygon pongen i lelig än trel, memil lede antent och psykocism epud lagir för
+                  lada. Pätärat presper, i hust netugisk mytodiktisk häkuss som biopur i geling pitt vartad vaplajigon
+                  tägåst och hypodalogi. Såsoning åren tektiga bioment, eumisamma, sunäligen med astropp om kvasiras,
+                  läslov, lapp dabesk om las. Nina tesm tok tekubel epiras, i dissade engar stenolans: miktig didorad
+                  sosal.
+                </p>
+              </div>
+            </div>
+            <div className="  h-[70%]   ">
+              <div className="mb-2 lg:mb-0 ">
+                <div>
+                  <Image
+                    src="/volunteer-force.png"
+                    alt=""
+                    width="100%"
+                    height="50vh"
+                    layout="responsive"
+                    objectFit="cover"
+                  />
+                </div>
+                <p className="text-black mt-8 leading-8">
+                  Lörem ipsum jen öling tede den geosorade lalig så sagen om terralog burkini om plagen, ifall vaktig
+                  inte leda krosäbelt. Myjygon pongen i lelig än trel, memil lede antent och psykocism epud lagir för
+                  lada. Pätärat presper, i hust netugisk mytodiktisk häkuss som biopur i geling pitt vartad vaplajigon
+                  tägåst och hypodalogi.
+                </p>
+              </div>
 
-const Volunteers = () => {
-  return <div>
-     <PageTitle name="Volunteers" />
-     <div className="mx-16">
-       <div className="pb-8  mb-16  border-b-2 border-[#F4C316]">
-     <div className="mt-16 mb-10 ">
-          <Image src="/leave_your_mark.png" alt="" width="100%" height="40vh" layout="responsive" objectFit="cover" />
+              <div className="flex flex-col gap-6   lg:h-[75%] mb-8     ">
+                <div className=" mt-5     ">
+                  <InnerSection
+                    title="Eurolig heteroskade. Nena tegen. Tehevar. "
+                    img="/volunteer-application.png"
+                    parag="Lörem ipsum jen öling tede den geosorade lalig så sagen om terralog burkini om plagen, ifall 
+            vaktig inte leda krosäbelt. "
+                    lower="Join now"
+                    arrow="/arrow 1.png"
+                  />
+                </div>
+
+                <div className="   ">
+                  <InnerSectionList
+                    title="Monoliga lener bede. Jutt lanas oaktat siktig."
+                    img="/volunteer-banner.png"
+                    list1="Tingar endoplastisk."
+                    list2=" Donade letungen en treter och "
+                    list3="Pararysk blingbling sperad."
+                   
+                  />
+                </div>
+                <div className="  ">
+                  <InnerSection
+                    title="Monoliga lener bede. Jutt lanas oaktat siktig."
+                    img="/fem-volunteers.png"
+                    parag="Todat spessa. Trelott osyr digår. Hexang miska vol. Kronat 
+            pebel tisat. Sasam mikrosat pabel. Rev
+             avonade vas. Synskop ira: och monoledes."
+                    lower="Get your certificate"
+                    arrow="/arrow 1.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="hidden lg:block w-4/12 xl:w-3/12  p-0">
+            <div>
+              <LatestNews />
+            </div>
+            <div>
+              <FixturesBrief />
+            </div>
+            <SideBar />
+          </div>
         </div>
-        
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada ipsum sed nisi gravida gravida.
-           Donec dapibus, tortor eget posuere pretium, lacus nibh lobortis odio, eget elementum turpis nisi in
-            purus. In efficitur lorem risus, ut pharetra purus scelerisque a. Nam in mattis lacus. Suspendisse 
-            dui leo, posuere id condimentum ac, pellentesque at erat. Pellentesque habitant morbi tristique 
-            senectus et netus et malesuada fames ac turpis egestas. Donec sed nulla pulvinar, vestibulum lacus 
-            ac, sollicitudin ipsum. Nulla quis sapien diam. 
-          Vivamus dictum, est ac interdum fermentum, nunc ipsum egestas arcu, in convallis purus neque ut ipsum.
-           </p>
-           </div>
-           {/* <hr className="pb-8 mx-4 mb-16  border-b-2 border-[#F4C316]"/> */}
-           
-           <div className="flex justify-between align-top border-b-2 border-b-[#F4C316] mb-16 pb-16   ">
-           <div className="w-[48%] ">
-             {/* <Image src="/volunteer-certificate.png" alt="" width="100%" height="75vh" layout="responsive" objectFit="cover" /> 
-             */}
-              <img src="/volunteer-application.png" className="h-[100%] w-[100%]" alt=""/>
-             </div>
-             <div className="flex flex-col w-1/2 mt-0 align-top justify-start  ">
-            
-               <h1 className="text-2xl mb-2 font-bold w-4/5 text mt-0 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h1>
-               <p> Donec dapibus, tortor eget posuere pretium, lacus nibh lobortis odio, eget elementum turpis nisi
-                  in purus. In efficitur lorem risus, ut pharetra purus scelerisque a. 
-                 Nam in mattis lacus. Suspendisse dui leo, posuere id condimentum ac, pellentesque at erat. </p>
-             </div>
-           </div>
-         
-           <div className="flex justify-between align-top border-b-2 border-b-[#F4C316] mb-16 pb-16 ">
-             <div className="flex flex-col align-top justify-between  w-[48%]">
-             <p >
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada ipsum sed nisi gravida gravida.
-              Donec dapibus, tortor eget posuere pretium, lacus nibh lobortis odio, eget elementum turpis nisi in 
-              purus. In efficitur lorem risus, ut pharetra purus scelerisque a. Nam in mattis lacus. Suspendisse
-               dui leo, posuere id condimentum ac, pellentesque at erat. Pellentesque habitant morbi tristique 
-               senectus et netus et malesuada fames ac turpis egestas. Donec sed nulla pulvinar, vestibulum lacu
-                ac, sollicitudin ipsum. Nulla quis sapien diam.
-              Vivamus dictum, est ac interdum fermentum, nunc ipsum egestas arcu, in convallis purus neque ut ipsum.
-             </p>
-             <button className="bg-black text-white w-1/2 h-16" >bbbb</button>
-             </div>
-             <div className="w-[48%] ">
-             {/* <Image src="/volunteer-certificate.png" alt="" width="100%" height="75vh" layout="responsive" objectFit="cover" /> 
-             */}
-              <img src="/volunteer-certificate.png" className="h-[100%] w-[100%]" alt=""/>
-             </div>
-           </div>
-
-           <div className="flex justify-between align-top border-b-1 border-b-[#F4C316] mb-16 pb-16 ">
-           <div className="w-[48%] ">
-             
-              <img src="/volunteer-banner.png" className="h-[100%] w-[100%]" alt=""/>
-             </div>
-             <div className="flex flex-col align-top justify-between  w-[48%]">
-             <p >
-             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada ipsum sed nisi gravida gravida.
-              Donec dapibus, tortor eget posuere pretium, lacus nibh lobortis odio, eget elementum turpis nisi in 
-              purus. In efficitur lorem risus, ut pharetra purus scelerisque a. Nam in mattis lacus. Suspendisse
-               dui leo, posuere id condimentum ac, pellentesque at erat. Pellentesque habitant morbi tristique 
-               senectus et netus et malesuada fames ac turpis egestas. Donec sed nulla pulvinar, vestibulum lacu
-                ac, sollicitudin ipsum. Nulla quis sapien diam.
-              Vivamus dictum, est ac interdum fermentum, nunc ipsum egestas arcu, in convallis purus neque ut ipsum.
-             </p>
-             <button className="bg-black text-white w-1/2 h-16">bbbb</button>
-             </div>
-             
-           </div>
-        
-
+      </div>
     </div>
-    </div>;
+  );
 };
 
-export default Volunteers;
+export default volunteer;
