11f8380e22b9b45602e6657e014e7fc44cbaebc5
Joshua Alexander <alexander.joshua2018@gmail.com>
2022-06-06 11:59:27 +0100

    fix issues

--- components/teams/Tabs.jsx
+++ components/teams/Tabs.jsx
@@ -5,7 +5,7 @@ const tabItems = ["Overview", "Squad", "Fixtures", "Results", "Stats", "Store"];
 const Tabs = ({ tabs, setTabs }) => {
   return (
     <div className="max-w-[94%] md:max-w-[90%] mx-auto text-black mt-3 font-redhat">
-      <div className="flex items-center justify-between gap-5 md:gap-10 overflow-scroll lg:overflow-hidden scrollbar-thin scrollbar-thumb-blue-900 scrollbar-track-gray-200 h-28">
+      <div className="flex items-center justify-between gap-5 md:gap-10 overflow-scroll lg:overflow-hidden scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-200 h-28">
         {tabItems?.map((tab, idx) => (
           <div
             key={idx}
