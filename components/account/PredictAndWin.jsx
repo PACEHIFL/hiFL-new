8d06149f9eb56ca5c53632e5aad863368ceb4e59
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-06-09 00:13:09 +0100

    store landing page

--- components/account/PredictAndWin.jsx
+++ components/account/PredictAndWin.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import AccountLayout from "../layout/AccountLayout";
+import Predictions from "./predictandwin/Predictions";
+import PredictTabs from "./predictandwin/predictTabs";
 
 const predictItems = [
   {
@@ -49,6 +51,7 @@ const predictItems = [
 ];
 
 const PredictAndWin = () => {
+  const [activeTab, setActiveTab] = useState(0);
   const [data, setData] = useState(predictItems);
 
   const handleClick = (type, name) => {
@@ -60,61 +63,15 @@ const PredictAndWin = () => {
   };
   return (
     <AccountLayout name="predict & win">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
-        {data?.map(
-          ({ stage, date, time, homeTeamLogo, homeTeamName, homeScore, awayTeamLogo, awayTeamName, awayScore }, i) => (
-            <div className="bg-white rounded-md p-4" key={i}>
-              <div className="flex justify-between text-sm">
-                <div className="flex gap-2">
-                  <img src="/hifl-icon.png" alt="HiFL" />
-                  <h2>{stage}</h2>
-                </div>
-                <div className="flex gap-2">
-                  <h2>{date}</h2>
-                  <span>|</span>
-                  <h2>{time}</h2>
-                </div>
-              </div>
-              <div className="flex justify-center my-8">
-                <div className="flex gap-8 items-center text-sm">
-                  <div className="flex gap-6 items-center">
-                    <div className="flex flex-col gap-2 text-center items-center justify-center">
-                      <img src={homeTeamLogo} alt={homeTeamName} className="w-[40px]" />
-                      <h2>{homeTeamName}</h2>
-                    </div>
-                    <div className="flex flex-col gap-2 items-center">
-                      <img src="/top-arrow.png" alt="" className="w-[18px] cursor-pointer" onClick={handleClick} />
-                      <span className="p-3 border border-secondary">{homeScore}</span>
-                      <img src="/bottom-arrow.png" alt="" className="w-[18px] cursor-pointer" onClick={handleClick} />
-                    </div>
-                  </div>
-                  <span>V</span>
-                  <div className="flex gap-6 items-center">
-                    <div className="flex flex-col gap-2 items-center">
-                      <img
-                        src="/top-arrow.png"
-                        alt=""
-                        className="w-[18px] cursor-pointer"
-                        onClick={() => handleClick("increase", awayScore)}
-                      />
-                      <span className="p-3 border border-secondary">{awayScore}</span>
-                      <img
-                        src="/bottom-arrow.png"
-                        alt=""
-                        className="w-[18px] cursor-pointer"
-                        onClick={() => handleClick("decrease")}
-                      />
-                    </div>
-                    <div className="flex flex-col gap-2 text-center items-center justify-center">
-                      <img src={awayTeamLogo} alt={awayTeamName} className="w-[40px]" />
-                      <h2>{awayTeamName}</h2>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div className="p-3 md:p-6">
+        <div className="md:p-6 bg-whit">
+          <div className="bg-white px-4 py-8">
+            <div className="border-b border-warning">
+              <PredictTabs activeTab={activeTab} setActiveTab={setActiveTab} />
             </div>
-          )
-        )}
+          </div>
+          <div className="pt-6">{activeTab === 0 && <Predictions data={data} handleClick={handleClick} />}</div>
+        </div>
       </div>
     </AccountLayout>
   );
--- pages/store/index.js
+++ pages/store/index.js
@@ -1,7 +1,132 @@
 import React from "react";
+import StoreLayout from "../../components/layout/StoreLayout";
+import Link from "next/link";
+import { formatMoney } from "../../helpers/utils";
+import ProductCard from "../../components/store/ProductCard";
 
 const Store = () => {
-  return <div>index</div>;
+  const products = [
+    {
+      id: 234,
+      name: "UNN Lions Home Authentic Shirt",
+      bestSelling: false,
+      img: "/hiversa-jersey.png",
+      discountPercent: 10,
+      discountPrice: null,
+      price: 5225,
+    },
+    {
+      id: 5553,
+      name: "UNN Lions Home Authentic Shirt",
+      bestSelling: true,
+      img: "/hiversa-jersey.png",
+      discountPercent: 10,
+      discountPrice: 4225,
+      price: 5225,
+    },
+    {
+      id: 45445,
+      name: "UNN Lions Home Authentic Shirt",
+      bestSelling: true,
+      img: "/hiversa-jersey.png",
+      discountPercent: 10,
+      discountPrice: 4225,
+      price: 5225,
+    },
+    {
+      id: 5553,
+      name: "UNN Lions Home Authentic Shirt",
+      bestSelling: true,
+      img: "/hiversa-jersey.png",
+      discountPercent: 10,
+      discountPrice: 4225,
+      price: 5225,
+    },
+    {
+      id: 45445,
+      name: "UNN Lions Home Authentic Shirt",
+      bestSelling: true,
+      img: "/hiversa-jersey.png",
+      discountPercent: 10,
+      discountPrice: 4225,
+      price: 5225,
+    },
+    {
+      id: 5553,
+      name: "UNN Lions Home Authentic Shirt",
+      bestSelling: true,
+      img: "/hiversa-jersey.png",
+      discountPercent: 10,
+      discountPrice: 4225,
+      price: 5225,
+    },
+    {
+      id: 45445,
+      name: "UNN Lions Home Authentic Shirt",
+      bestSelling: true,
+      img: "/hiversa-jersey.png",
+      discountPercent: 10,
+      discountPrice: 4225,
+      price: 5225,
+    },
+  ];
+  return (
+    <StoreLayout>
+      <div className="mb-10">
+        <div className="mb-6">
+          <Link href="#">
+            <a>
+              <img src="/store-banner1.png" alt="" className="w-full" />
+            </a>
+          </Link>
+        </div>
+
+        <div className="flex flex-col md:flex-row gap-4">
+          <Link href="#">
+            <a className="">
+              <img src="/store-banner2.png" alt="" className="w-full md:w-auto" />
+            </a>
+          </Link>
+          <Link href="#">
+            <a className="">
+              <img src="/store-banner3.png" alt="" className="w-full md:w-auto" />
+            </a>
+          </Link>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex justify-between items-center border-b border-warning pb-1 mb-2">
+          <h2 className="text-lg xl:text-xl font-semibold">Shop your favourite merchandize</h2>
+          <div className="flex gap-2 items-center text-sm">
+            <h3>Sort by:</h3>
+            <div className="flex gap-1 items-center cursor-pointer">
+              <div className="dropdown xl:dropdown-hover dropdown-end">
+                <label tabindex="0" className="m-1 text-[#767575] cursor-pointer">
+                  Category
+                </label>
+                <ul tabindex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box divide-y">
+                  <li className="p-2">All</li>
+                  <li className="p-2">Kits</li>
+                  <li className="p-2">Equipment</li>
+                  <li className="p-2">Wearables</li>
+                </ul>
+              </div>
+              <div>
+                <img src="/stroke-down.png" alt="" className="w-[12px]" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <p>{products.length} Kits found</p>
+        <div className="mt-2 pt-16 pb-6 border-t border-[#AFAFAF] grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 text-secondary">
+          {products.map((product, i) => (
+            <ProductCard product={product} key={i} />
+          ))}
+        </div>
+      </div>
+    </StoreLayout>
+  );
 };
 
 export default Store;
