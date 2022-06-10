c1d959989f4c2c5e292cd8d2d7739548251592bc
Joshua Alexander <alexander.joshua2018@gmail.com>
2022-06-10 18:07:35 +0100

    completed terms page

--- pages/faqs/index.js
+++ pages/faqs/index.js
@@ -4,17 +4,18 @@ import PageTitle from "../../components/shared/PageTitle";
 import SideBar from "../../components/shared/SideBar";
 
 const Faqs = () => {
-  return <div>
-    <PageTitle name="FAQs" />
+  return (
+    <div>
+      <PageTitle name="FAQs" />
 
-     <div className="bg-white font-red-hat">
+      <div className="bg-white font-redhat">
         <div className="max-w-[94%] md:max-w-[90%] mx-auto py-10 text-black">
           <div className="flex gap-7 xl:gap-20 justify-between">
             <div className="w-full lg:w-8/12 xl:w-9/12">
               <div>
-                <h1 className="text-[36px] font-bold">HiFL</h1>
+                <h1 className="text-[36px] font-bold">HiFL®</h1>
 
-                <div> 
+                <div>
                   <Accordion />
                 </div>
               </div>
@@ -27,7 +28,8 @@ const Faqs = () => {
           </div>
         </div>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default Faqs;
