5928aec59d77d0b2a9edfe5cbacf32d69089daee
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-06-18 02:46:52 +0100

    video and gallery post details page done

--- components/layout/PostsDetailsLayout.jsx
+++ components/layout/PostsDetailsLayout.jsx
@@ -1,10 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import PageTitle from "../shared/PageTitle";
 import SideBar from "../shared/SideBar";
 import FixturesBrief from "../shared/FixturesBrief";
 import LatestNews from "../shared/LatestNews";
+import ReactPlayer from "react-player";
 
-const PostsDetailsLayout = ({ name, post: { Title, CoverImage, SubTitle, Content } }) => {
+const PostsDetailsLayout = ({ name, post: { Title, CoverImage, SubTitle, Content, Type, Media } }) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const [pageLoaded, setPageLoaded] = useState(false);
+
+  //convert media array to a format required by react-photo-gallery
+  const getPhotos = () => {
+    const photos = [];
+    for (let [i, photo] of Media.entries()) {
+      photos.push({
+        src: photo.url,
+        width: i == 0 ? 12 : 6,
+        height: 3,
+      });
+    }
+    return photos;
+  };
+  //open mage lightboox
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+  //close mage lightboox
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  useEffect(() => {
+    setPageLoaded(true);
+  }, []);
+
   return (
     <div className="font-redhat">
       <PageTitle name={name || "add a title"} />
@@ -13,10 +47,42 @@ const PostsDetailsLayout = ({ name, post: { Title, CoverImage, SubTitle, Content
           <div className="flex gap-10 xl:gap-20 justify-between">
             <div className="w-full lg:w-8/12 xl:w-9/12">
               <div className="">
-                <h2 className="text-2xl xl:text-3xl mb-2 font-semibold">{Title}</h2>
+                <h2 className="text-xl lg:text-2xl xl:text-3xl mb-2 font-semibold">{Title}</h2>
                 {SubTitle && <h3 className="text-accent text-base ">{SubTitle}</h3>}
                 <div className="mt-5">
-                  <img src={CoverImage.url} alt={Title} className="w-full" />
+                  {Type == "Default" && <img src={CoverImage.url} alt={Title} className="w-full" />}
+
+                  {/* video post */}
+                  {Type == "Video" && pageLoaded && Media && (
+                    <ReactPlayer
+                      url={Media?.[0].url}
+                      controls={true}
+                      // light={CoverImage.url}
+                      width="100%"
+                      // playIcon={<img src="/play.png" alt="" width="10%" />}
+                    />
+                  )}
+
+                  {/* gallery post */}
+                  {Type == "Gallery" && pageLoaded && Media && (
+                    <>
+                      <Gallery photos={getPhotos()} onClick={openLightbox} />
+                      <ModalGateway>
+                        {viewerIsOpen ? (
+                          <Modal onClose={closeLightbox}>
+                            <Carousel
+                              currentIndex={currentImage}
+                              views={getPhotos()?.map((x) => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title,
+                              }))}
+                            />
+                          </Modal>
+                        ) : null}
+                      </ModalGateway>
+                    </>
+                  )}
                 </div>
                 <div
                   dangerouslySetInnerHTML={{ __html: Content }}
