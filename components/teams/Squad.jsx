89b20276c2e66ece400b9232abb3d0ad46f978c2
Joshua Alexander <alexander.joshua2018@gmail.com>
2022-06-20 15:05:32 +0100

    Updates Teams Page

--- components/teams/Squad.jsx
+++ components/teams/Squad.jsx
@@ -7,11 +7,6 @@ const Squad = ({ data }) => {
   console.log(data);
   return (
     <div className="font-redhat">
-      <div className="p-4 border rounded-md bg-white flex flex-col my-4">
-        <span className="text-red-600">Filter by season</span>
-        <span className="font-bold">2022</span>
-      </div>
-
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
         {data.length != 0 ? (
           data?.map((player, idx) => <TeamPlayer key={idx} player={player} />)
--- components/teams/TeamPlayer.jsx
+++ components/teams/TeamPlayer.jsx
@@ -4,36 +4,38 @@ import Link from "next/link";
 
 const TeamPlayer = ({ player }) => {
   return (
-    <div className="bg-white rounded border shadow mb-2">
+    <div className="bg-white rounded border shadow mb-10">
       <div className="mb-1 bg-[url('/team-bg.png')] bg-cover bg-center flex items-center">
         <div className="rounded  h-[147px] w-full mb-4 flex flex-col justify-end text-secondary font-redhat pl-5">
-          <p className="font-bold capitalize md:text-lg">{player?.User?.Firstname} {player?.User?.Lastname}</p>
+          <p className="font-bold capitalize md:text-lg">
+            {player?.User?.Firstname} {player?.User?.Lastname}
+          </p>
           <p>{player?.User?.Role}</p>
         </div>
         <div className="flex justify-end h-36 w-48 mr-3 bg-white p-1">
-          <img src={player?.DocumentUploads?.PassportPhotograph}  alt="picture" className="w-full h-full object-cover" />
+          <img src={player?.DocumentUploads?.PassportPhotograph} alt="picture" className="w-full h-full object-cover" />
         </div>
       </div>
 
       <div className="text-sm font-redhat">
         <div className="flex justify-between items-center py-1 px-3 border-b">
-          <p>Apperances</p>
-          <p>24</p>
+          <p>Course</p>
+          <p>{player?.AcademicRecord?.CourseStudy}</p>
         </div>
         <div className="flex justify-between items-center py-1 px-3 border-b">
-          <p>Goals</p>
-          <p>10</p>
+          <p>Level</p>
+          <p>{player?.AcademicRecord?.CourseLevel}</p>
         </div>
       </div>
 
-      <div className="flex justify-end text-sm">
+      {/* <div className="flex justify-end text-sm">
         <Link href="#">
           <a className="flex gap-2 py-2 px-3 font-redhat">
             <span>Player Profile</span>
             <Image src="/right-arrow.png" alt="" width={11} height={14} />
           </a>
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 };
