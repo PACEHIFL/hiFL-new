720619f54430fd20e73b17e9a8bbe4ee784b0f7e
Joshua Alexander <alexander.joshua2018@gmail.com>
2022-06-23 21:18:30 +0100

    Updates teams page

--- components/teams/AllTeams.jsx
+++ components/teams/AllTeams.jsx
@@ -1,66 +1,131 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Filter from "./Filter";
 import PageTitle from "../shared/PageTitle";
 import SideBar from "../shared/SideBar";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { ChevronRight } from "@heroicons/react/outline";
+import axios from "axios";
+import { BeatLoader } from "react-spinners";
 
-const AllTeams = ({ data, allSeasons }) => {
-  const [AllSeasons, setAllSeasons] = useState(allSeasons);
-  const initialState = { CurrentSeason: AllSeasons, CurrentLeague: "", CurrentStage: "" };
-  const [seasonDetails, setSeasonDetails] = useState(initialState);
+const AllTeams = ({ settings: allSettings, seasons }) => {
+  const [settings, setSettings] = useState(allSettings);
+  const [teams, setTeams] = useState([]);
+  const [stages, setStages] = useState([]);
+  const [leagues, setLeagues] = useState([]);
+  const [currentStageId, setCurrentStageId] = useState('')
+  const [loading, setLoading] = useState(false)
   const isOdd = (num) => num % 2 === 0;
   const router = useRouter();
   const path = router.pathname;
+  const baseURL = process.env.BASE_URL;
+
+  const fetchStages = async () => {
+    setLoading(true)
+    try {
+      const { data: allStages } = await axios(`${baseURL}/leagues/league/stages/?League=${settings.CurrentLeague._id}`);
+      setStages(allStages?.data);
+      setLoading(false)
+
+      if(allStages) {
+        setCurrentStageId(allStages?.data[0]?._id)
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  const fetchTeams = async () => {
+    setLoading(true)
+    try{
+      const { data } = await axios(`${baseURL}/leagues/league/stages/stage/?_id=${currentStageId}`);
+      setTeams(data?.data?.Teams)
+      setLoading(false)
+    }catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchStages()
+  }, []);
+
+  useEffect(() => {
+    if(currentStageId !== '') {
+      fetchTeams()
+    }
+  }, [currentStageId]);
 
   const handleChange = (e) => {
-    e.preventDefault();
+    const { value } = e.target
+    setCurrentStageId(value)
   };
+
+  const handleSeasonChange = (e) => {
+    const { name, value } = e.target
+    // setLoading(true)
+    // console.log(value, settings.CurrentSeason._id)
+    // if(value !== settings.CurrentSeason._id) {
+    //   setTeams([])
+    //   setLoading(false)
+    // } else {
+    //   setTeams([...teams])
+    // }
+  }
 
   return (
     <div>
       <PageTitle name="Teams" />
-      <div className="bg-white font-red-hat">
+      <div className="bg-white font-redhat">
         <div className="max-w-[94%] md:max-w-[90%] mx-auto py-10 text-black">
           <div className="flex gap-7 xl:gap-20 justify-between">
             <div className="w-full lg:w-8/12 xl:w-9/12">
               <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-auto mb-10">
-                <Filter
-                  title="Select Season"
-                  onChange={handleChange}
-                  name="CurrentSeason"
-                  seasonDetails={seasonDetails}
-                />
-                <Filter title="Select League" />
-                <Filter title="Select Stage" />
+                <Filter title="Select Season" onChange={handleSeasonChange} name="CurrentSeason" seasons={seasons} />
+                <select
+                  className="select w-full border-gray-500"
+                  name='hello'
+                  onChange={handleChange}>
+                  {stages &&
+                    stages?.map((stage, idx) => (
+                      <option className="text-red-600" key={idx} value={stage?._id}>
+                        {stage?.StageName}
+                      </option>
+                    ))}
+                </select>
               </div>
 
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {data &&
-                  data?.map((team, idx) => (
+              {loading && (
+                <div className="h-[400px] flex justify-center items-center">
+                  <BeatLoader loading={loading} color="#000229" />
+                </div>
+              )}
+
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 font-redhat">
+                {teams &&
+                  teams.map((team, idx) => (
                     <Link href={`${path}/${team._id}`} key={idx}>
                       <div
                         className={`${
                           isOdd(idx) ? "bg-[url('/team-bg-blue.png')]" : "bg-[url('/team-bg-red.png')]"
-                        } bg-cover bg-no-repeat py-4 px-6 cursor-pointer`}>
-                        <div className="rounded-full w-20 my-4 bg-white">
+                        } bg-cover bg-no-repeat py-4 px-6 cursor-pointer rounded-md`}>
+                        <div className="rounded-full w-20 h-20 my-4 border-2 bg-white">
                           <img
-                            src={`${team.schoolLogo ? team.schoolLogo : "/default-team-logo.png"}`}
+                            src={`${team.TeamLogo ? team.TeamLogo : "/default-team-logo.png"}`}
                             alt={team.TeamName}
-                            className="w-full"
+                            className="w-full rounded-full h-full"
                           />
                         </div>
                         <div>
-                          <h1 className="text-yellow-400 font-semibold text-2xl">
-                            {team?.Institution?.InstitutionName}
-                          </h1>
+                          <h1 className="text-yellow-400 font-bold text-2xl">{team?.Institution?.InstitutionName}</h1>
                           <h1 className="font-semibold text-white mt-3 text-xl">{team.TeamName}</h1>
-                          <span className="text-white mt-4 flex gap-3">View Team</span>
+                          <span className="text-white mt-4 flex gap-3 hover:text-warning">View Team</span>
                         </div>
                       </div>
                     </Link>
                   ))}
+
+                  {teams.length === 0 && <h1> No Team available for the stage </h1>}
               </div>
             </div>
             <div className="hidden lg:block w-4/12 xl:w-3/12">
--- pages/teams/[id]/index.js
+++ pages/teams/[id]/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Fixtures from "../../../components/teams/Fixtures";
 import Overview from "../../../components/teams/Overview";
 import Results from "../../../components/teams/Result";
@@ -9,63 +9,90 @@ import TeamsHeader from "../../../components/teams/TeamsHeader";
 import Stats from "../../../components/teams/Stats";
 import Store from "../../../components/teams/Store";
 import axios from "axios";
+import { useRouter } from "next/router"
 
-const Slug = ({ data, players }) => {
+const Slug = () => {
   const [tabs, setTabs] = useState(0);
+  const [team, setTeam] = useState({});
+  const [loading, setLoading] = useState(false)
+  const [players, setPlayers] = useState([]);
+  const [fixtures, setFixtures] = useState([]);
+  const [results, setResults] = useState([]);
+  const router = useRouter()
+  const pathName = router.query.id
+
+  const fetchData = async () => {
+    setLoading(true)
+    const baseURL = process.env.BASE_URL;
+    const { data, errors } = await axios(`${baseURL}/teams/team/?_id=${pathName}`);
+    const { data: player } = await axios(`${baseURL}/players/active/?Team=${pathName}`);
+    const { data: fixtures } = await axios(`${baseURL}/leagues/season/fixtures/?MatchStatus=FIXTURE&HomeTeam=${pathName}`);
+    const { data: results } = await axios(`${baseURL}/leagues/season/fixtures/?MatchStatus=RESULT&HomeTeam=${pathName}`);
+    setTeam(data?.data)
+    setPlayers(player?.data)
+    setFixtures(fixtures?.data)
+    setResults(results?.data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    if(pathName) {
+      fetchData()
+    }
+  }, [pathName])
+
   return (
     <div>
-      <TeamsHeader data={data} />
+      <TeamsHeader data={team} />
       <Tabs tabs={tabs} setTabs={setTabs} />
       <div className="bg-white">
-        <div className="max-w-[94%] md:max-w-[90%] mx-auto py-10 text-black">
-          {tabs === 0 && <Overview data={data} />}
-          {tabs === 1 && <Squad data={players} />}
-          {tabs === 2 && <Fixtures />}
-          {tabs === 3 && <Results data={data} />}
-          {/* {tabs === 4 && <h1 className="font-redhat text-2xl"> There is no stats available at the moment </h1>} */}
-          {/* {tabs === 4 && <Stats data={data} />} */}
-          {tabs === 4 && <Store data={data} />}
+        <div className="max-w-[94%] md:max-w-[90%] mx-auto py-10 text-black"> 
+          {tabs === 0 && <Overview data={team} loading={loading} />}
+          {tabs === 1 && <Squad data={players} loading={loading} />}
+          {tabs === 2 && <Fixtures data={fixtures} loading={loading} />}
+          {tabs === 3 && <Results data={results} loading={loading} />}
+          {/* {tabs === 4 && <Stats data={data} /> } */}
+          {tabs === 4 && <Store /> }
         </div>
       </div>
     </div>
   );
 };
+
 export default Slug;
 
-export const getStaticProps = async ({ params: { id } }) => {
-  try {
-    const baseURL = process.env.BASE_URL;
-    const { data, errors } = await axios(`${baseURL}/teams/team/?_id=${id}`);
-    const { data: players } = await axios(`${baseURL}/players/active/?Team=${id}`);
+// export const getStaticProps = async ({ params: { id } }) => {
+//   try {
+//     const baseURL = process.env.BASE_URL;
+//     const { data, errors } = await axios(`${baseURL}/teams/team/?_id=${id}`);
+//     const { data: players } = await axios(`${baseURL}/players/active/?Team=${id}`);
 
-    if (!data || errors) {
-      return { notFound: true };
-    }
+//     return {
+//       props: {
+//         data: data.data,
+//         players: players.data,
+//       },
+//     };
+//   } catch (error) {
+//     // return { notFound: true };
+//     console.log(error)
+//   }
+// };
 
-    return {
-      props: {
-        data: data.data,
-        players: players.data,
-      },
-    };
-  } catch (error) {
-    return { notFound: true };
-  }
-};
+// export const getStaticPaths = async () => {
+//   try {
+//     const baseURL = process.env.BASE_URL;
+//     const { data: settings, errors } = await axios(`${baseURL}/settings/setting/league/?CurrentLeagueName=HiFL`);
+//     const { data } = await axios(`${baseURL}/leagues/league/stages/?League=${settings?.data?.CurrentLeague?._id}`);
+//     const ids = data?.data?.map((team) => team.Teams).map(team => console.log([...team]))
+//     const paths = ids.map((id) => ({ params: { id: id + "" } }))
 
-export const getStaticPaths = async () => {
-  try {
-    const baseURL = process.env.BASE_URL;
-    const { data } = await axios(`${baseURL}/teams/all`);
-
-    const ids = data.data.map((team) => team._id);
-    const paths = ids.map((id) => ({ params: { id: id + "" } }));
-
-    return {
-      paths,
-      fallback: false,
-    };
-  } catch (error) {
-    return { paths: [], fallback: false };
-  }
-};
+    
+//     return {
+//       paths,
+//       fallback: false,  
+//     }
+//   } catch (error) {
+//     return { paths: [], fallback: false };
+//   }
+// };
