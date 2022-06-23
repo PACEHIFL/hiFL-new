265d15c92090c4b5c1f225df051ce5a5dc690026
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-06-24 00:00:51 +0100

    fixed build error in filter.jsx

--- components/teams/Filter.jsx
+++ components/teams/Filter.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Filter = ({ seasons, onChange }) => {
   return (
     <select className="select w-full border-gray-500" name="CurrentSeason" onChange={onChange}>
-      {seasons && seasons?.map((season, idx) => (
-        <option value={season?._id} className="text-red-600">
-          {season?.SeasonYear}
-        </option>
-      ))}
-     
+      {seasons &&
+        seasons?.map((season, idx) => (
+          <option value={season?._id} className="text-red-600" key={idx}>
+            {season?.SeasonYear}
+          </option>
+        ))}
     </select>
   );
 };
