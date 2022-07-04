4f593d4b01f5fd26139d38ee9b1c973341354788
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-07-05 00:24:09 +0100

    customize only customizable products

--- components/store/ProductInfo.jsx
+++ components/store/ProductInfo.jsx
@@ -14,6 +14,8 @@ const ProductInfo = ({ data, orderInfo, setOrderInfo, initialState, handleQuanti
   const router = useRouter();
   const dispatch = useDispatch();
 
+  console.log(data, "product");
+
   //const { Price, DiscountPrice, InStock, PreOrder, Details, ProductGallery } = data;
 
   const handleAddToCart = (e) => {
@@ -105,29 +107,33 @@ const ProductInfo = ({ data, orderInfo, setOrderInfo, initialState, handleQuanti
         </div>
       </div>
       <div>
-        <h3 className="text-[#7E7E7E] text-xs font-semibold uppercase mb-2">Customize</h3>
-        <div className="flex gap-3 items-center text-[#7E7E7E]" onChange={handleChange}>
-          <div>
-            <input type="radio" name="customize" value="Yes" className="hidden" ref={customizeYes} />
-            <p
-              className={`${
-                orderInfo.customize == "Yes" && "bg-[#7E7E7E] text-white"
-              } px-4 py-2 border border-[#7E7E7E80] border-opacity-50 rounded-md cursor-pointer select-none uppercase`}
-              onClick={() => customizeYes.current.click()}>
-              Yes
-            </p>
-          </div>
-          <div>
-            <input type="radio" name="customize" value="No" className="hidden" ref={customizeNo} />
-            <p
-              className={`${
-                orderInfo.customize == "No" && "bg-[#7E7E7E] text-white"
-              } px-4 py-2 border border-[#7E7E7E80] border-opacity-50 rounded-md cursor-pointer select-none uppercase`}
-              onClick={() => customizeNo.current.click()}>
-              No
-            </p>
-          </div>
-        </div>
+        {data?.Customizable && (
+          <>
+            <h3 className="text-[#7E7E7E] text-xs font-semibold uppercase mb-2">Customize</h3>
+            <div className="flex gap-3 items-center text-[#7E7E7E]" onChange={handleChange}>
+              <div>
+                <input type="radio" name="customize" value="Yes" className="hidden" ref={customizeYes} />
+                <p
+                  className={`${
+                    orderInfo.customize == "Yes" && "bg-[#7E7E7E] text-white"
+                  } px-4 py-2 border border-[#7E7E7E80] border-opacity-50 rounded-md cursor-pointer select-none uppercase`}
+                  onClick={() => customizeYes.current.click()}>
+                  Yes
+                </p>
+              </div>
+              <div>
+                <input type="radio" name="customize" value="No" className="hidden" ref={customizeNo} />
+                <p
+                  className={`${
+                    orderInfo.customize == "No" && "bg-[#7E7E7E] text-white"
+                  } px-4 py-2 border border-[#7E7E7E80] border-opacity-50 rounded-md cursor-pointer select-none uppercase`}
+                  onClick={() => customizeNo.current.click()}>
+                  No
+                </p>
+              </div>
+            </div>
+          </>
+        )}
 
         <form className="mt-4 space-y-6 " onSubmit={handleAddToCart}>
           {orderInfo.customize == "Yes" && (
--- pages/store/[id].js
+++ pages/store/[id].js
@@ -124,38 +124,3 @@ const SingleProduct = () => {
 };
 
 export default SingleProduct;
-
-// export const getStaticProps = async ({ params: { id } }) => {
-//   try {
-//     const baseURL = process.env.CMS_URL;
-//     const { data, errors } = await axios(`${baseURL}/products/${id}?populate=*`);
-
-//     if (errors || !data) {
-//       return { notFound: true };
-//     }
-
-//     return {
-//       props: {
-//         data: data,
-//       },
-//     };
-//   } catch (error) {
-//     return { notFound: true };
-//   }
-// };
-
-// export const getStaticPaths = async () => {
-//   try {
-//     const baseURL = process.env.CMS_URL;
-//     const { data } = await axios(`${baseURL}/products?populate=*`);
-
-//     const ids = data.data.map((post) => post.id);
-//     const paths = ids.map((id) => ({ params: { id: id.toString() } }));
-//     return {
-//       paths,
-//       fallback: true,
-//     };
-//   } catch (error) {
-//     return { paths: [], fallback: false };
-//   }
-// };
